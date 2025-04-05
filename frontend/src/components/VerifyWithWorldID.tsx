119c31e36781387c13474193a8b315f09ba37843
DORA_XIE <joy8963259@gmail.com>
2025-04-05 17:17:14 +0800

    chore: update padding

--- frontend/src/components/VerifyWithWorldID.tsx
+++ frontend/src/components/VerifyWithWorldID.tsx
@@ -69,7 +69,7 @@ const VerifyWithWorldID = ({
 
       {showSuccessDialog && (
         <div className="fixed inset-0 bg-gray-600/30 bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
+          <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full m-10">
             <h2 className="text-xl font-bold mb-4">Verification successful!</h2>
             <p className="mb-6">You have successfully verified with World ID.</p>
             <button
